48f71b646959dca76dd1cefb189d080f9f180256
vimpal <vimpal.singh@gmail.com>
2016-11-15 11:37:06 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -7,13 +7,13 @@ module.exports = function(config) {
 
         files: [
             // paths loaded by Karma
-            {pattern: './node_modules/angular2/bundles/angular2-polyfills.js', included: true, watched: true},
+            {pattern: 'node_modules/angular2/bundles/angular2-polyfills.js', included: true, watched: true},
             {pattern: './node_modules/systemjs/dist/system.src.js', included: true, watched: true},
-            {pattern: './node_modules/rxjs/bundles/Rx.js', included: true, watched: true},
-            {pattern: './node_modules/angular2/bundles/angular2.dev.js', included: true, watched: true},
-            {pattern: './node_modules/angular2/bundles/testing.dev.js', included: true, watched: true},
-            {pattern: './node_modules/angular2/bundles/http.dev.js', included: true, watched: true},
-            {pattern: './karma-test-shim.js', included: true, watched: true},
+            {pattern: 'node_modules/rxjs/bundles/Rx.js', included: true, watched: true},
+            {pattern: 'node_modules/angular2/bundles/angular2.dev.js', included: true, watched: true},
+            {pattern: 'node_modules/angular2/bundles/testing.dev.js', included: true, watched: true},
+            {pattern: 'node_modules/angular2/bundles/http.dev.js', included: true, watched: true},
+            {pattern: 'karma-test-shim.js', included: true, watched: true},
 
             // paths loaded via module imports
             {pattern: 'dist/**/*.js', included: false, watched: true},
